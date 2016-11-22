6337510f230b2bb349fdf1d0a471b3168acf2159
uhura <uhura@10.0.1.11>
2016-11-22 10:32:38 +0700

    Initial Spring Boot project.

---
+++ src/main/java/com/namcha/Application.java
@@ -0,0 +1,16 @@
+package com.namcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by uhura on 11/22/2016 AD.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
