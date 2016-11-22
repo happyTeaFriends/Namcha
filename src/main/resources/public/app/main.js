e26c73fb43a6adb7f4406c8779d77aef830b2f22
user6 <aaeang@hotmail.com>
2016-11-22 17:14:07 +0700

    Add /api/list

--- src/main/resources/public/app/main.js
+++ src/main/resources/public/app/main.js
@@ -2,4 +2,8 @@ define(['jquery'], function ($) {
     $.get('api/hello', function(data) {
     	$('#response').html(data);
     })
+
+    $.get('api/list', function(data) {
+        	$('#response').html(data);
+    })
 });
