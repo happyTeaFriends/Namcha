a9a752118e902ea4a029948162cc0484ea347452
khongsak-k <uhura@10.0.1.11>
2016-11-23 16:09:56 +0700

    - Change property type from 'OffsetDateTime' to 'Date' in Content.java - Add @RequestBody into create() in ContentController.java - Add get() by id into ContentService.java

--- src/main/java/com/namcha/controllers/ContentController.java
+++ src/main/java/com/namcha/controllers/ContentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class ContentController {
 	private ContentService contentService;
 
 	@RequestMapping(method = RequestMethod.POST)
-	public Content create(Content content) {
+	public Content create(@RequestBody Content content) {
 		return contentService.create(content);
 	}
 
