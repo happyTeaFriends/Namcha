73b29b55bb886af9a2c707bcf53bc6c93f581e11
wimow168 <moodangkmutnb@gmail.com>
2016-11-23 16:13:47 +0700

    Implement controller's retrieve method.

--- src/main/java/com/namcha/controllers/ContentController.java
+++ src/main/java/com/namcha/controllers/ContentController.java
@@ -1,6 +1,5 @@
 package com.namcha.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +28,6 @@ public class ContentController {
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Content> getAll() {
 
-		Content content = new Content();
-		content.setEventName("Programming");
-
-		List<Content> contents = new ArrayList<Content>();
-		contents.add(content);
-		return contents;
+		return contentService.retrieve();
 	}
 }
