914aabd35a93f83246d2ea2c374b4a475ffcdf54
Khan <khan@Khans-Mac-mini.local>
2016-11-24 18:07:17 +0700

    add modal error when cannot retrieve data

--- src/main/resources/public/assets/js/common.js
+++ src/main/resources/public/assets/js/common.js
@@ -1,3 +1,19 @@
+function getParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? "" : sParameterName[1];
+        }
+    }
+    return "";
+}
+
 (function ($) {
     $.fn.serializeFormJSON = function () {
 
